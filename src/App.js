8cacc1a044ad797d693115652344f5734b732302
zilurrane <zilurrane@gmail.com>
2018-08-27 00:09:58 +0530

    Material React

--- src/App.js
+++ src/App.js
@@ -1,5 +1,40 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import { withStyles } from '@material-ui/core/styles'
+import Grid from '@material-ui/core/Grid'
+import Paper from '@material-ui/core/Paper'
 
-const App = () => <div>React Boilerplate!</div>
+const styles = theme => ({
+  root: {
+    flexGrow: 1
+  },
+  paper: {
+    padding: theme.spacing.unit * 2,
+    textAlign: 'center'
+  }
+})
 
-export default App
+class GuttersGrid extends React.Component {
+  render () {
+    const { classes } = this.props
+
+    return (
+      <Grid container className={classes.root} spacing={16}>
+        <Grid item xs={12}>
+          <Grid container justify='center' spacing={16}>
+            <h1>Zilu Ramkrishna Rane</h1>
+            <Grid item xs={12} sm={10}>
+              <Paper className={classes.paper}>Software Engineer</Paper>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    )
+  }
+}
+
+GuttersGrid.propTypes = {
+  classes: PropTypes.object.isRequired
+}
+
+export default withStyles(styles)(GuttersGrid)
