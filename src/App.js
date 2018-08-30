2d603c322951a38bd83d57bcb3b3093c49d661e5
zilurrane <zilurrane@gmail.com>
2018-08-30 09:34:04 +0530

    Cool Color

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const styles = theme => ({
   root: {
     flexGrow: 1,
     color: '#FFF',
-    backgroundColor: '#008080'
+    background: 'linear-gradient(150deg,#281483 15%,#8f6ed5 70%,#d782d9 94%)'
   },
   userName: {
     textAlign: 'center'
